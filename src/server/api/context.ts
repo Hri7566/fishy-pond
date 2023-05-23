f803bd18375e98c5cfae85310f2b86924a8cff03
Hri7566 <hri7566@gmail.com>
2023-05-22 22:29:13 -0400

    fix ips

--- src/server/api/context.ts
+++ src/server/api/context.ts
@@ -4,7 +4,8 @@ import { IDGenerator } from "../auth/IDGenerator";
 
 export const createContext = (opts: CreateFastifyContextOptions) => {
     // Get IP directly, or optionally from X-Forwarded-For if provided by a proxy service
-    let ip = opts.req.ip;
+    let ip = opts.req.socket.remoteAddress;
+    if (opts.req.ip) ip = opts.req.ip;
     if (opts.req.ips) ip = opts.req.ips[0];
 
     // Generate user ID
--- src/server/api/fastify.ts
+++ src/server/api/fastify.ts
@@ -5,19 +5,16 @@ import { env } from "../util/env";
 import FastifyWebSocket from "@fastify/websocket";
 
 // Create fastify server
-
 const fastify = Fastify({
     logger: env.NODE_ENV == "development"
 });
 
 // Serve static files
-
 fastify.register(FastifyStatic, {
     root: resolve("build/client")
 });
 
 // Enable websockets
-
 fastify.register(FastifyWebSocket);
 
 export default fastify;
